105e15b31741f0fd2a920b0acf7cf7a683ac0b57
Justine Imasiku <justineimasiku21@gmail.com>
2024-01-09 21:49:48 +0000

    Initialize app with flutter

---
+++ android/app/src/main/kotlin/com/example/nicetyfarm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nicetyfarm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:nicetyfarm/nicetyfarm_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
