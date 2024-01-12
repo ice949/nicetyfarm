eb6ab12564e418e55f1e568e15284c73802b74df
Justine Imasiku <justineimasiku21@gmail.com>
2024-01-12 01:17:22 +0000

    Create on boarding screen

--- lib/nicetyfarm_home_page.dart
+++ lib/nicetyfarm_home_page.dart
@@ -15,52 +15,42 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     
-    return Container(
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-         const Icon(
-            Icons.mic, size: 100, color: Colors.blue,
-            ),
-         const Text(
-            'Experience', style: TextStyle(fontSize: 30, color: Colors.blueGrey),
+    return Column(
+      children: [
+        const Expanded(child: Image(image: AssetImage('assets/images/onboard_icon.png'))),
+        const Padding(
+          padding: EdgeInsets.only(top: 0, bottom: 16.0, left: 16.0, right: 16.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text('WELCOME \n TO SANTORINI', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue), textAlign: TextAlign.center,),
+              ),
+              Text('Experience the beauty of Greece!', style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.black), textAlign: TextAlign.center,)
+            ],
           ),
-         const Text(
-            'Extended Reality', style: TextStyle(fontSize: 30, color: Colors.blueGrey),
-          ),
-         const Text(
-            'Experience the power of limitless possibilities', style: TextStyle(fontSize: 15, color: Colors.black),
-          ),
-          Row(
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 0, bottom: 16.0, left: 16.0, right: 16.0),
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              
-              ElevatedButton(
-                onPressed: () {}, 
-                child: const Text('Login'),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.red,
-                  onPrimary: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),),
-                ElevatedButton(
-                onPressed: () {}, 
-                child:const Text('Register'),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.white,
-                  onPrimary: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),),
-                
+              ElevatedButton(onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                minimumSize: Size(150, 50)),
+               child: Text('LOGIN', style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.white), textAlign: TextAlign.center, ),),
+               TextButton(onPressed: () {},
+               child: Text('REGISTER', style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.blue), textAlign: TextAlign.center, ),
+               )
             ],
-          )
-        ],
-      ),
+          ),
+        ),
+      ],
     );
-  }
+}
 }
